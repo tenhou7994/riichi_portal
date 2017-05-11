541d890ba297c334b19114a216277d1e06a02ded
Dmitry <dmitry.radionov@jetrockets.ru>
2017-05-12 00:10:52 +0300

    Gems and procs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,8 +25,12 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+
+  config.assets.compress = true
+  config.assets.compile = false
+  config.assets.digest = true
+  config.assets.js_compressor = :closure
+  config.assets.css_compressor = :yui
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
